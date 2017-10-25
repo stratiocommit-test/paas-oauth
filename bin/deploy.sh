43d55c7686898ed8339ac0d1c7b06432157b08c2
pbedia-stratio <pbedia@stratio.com>
2017-10-25 17:09:55 +0200

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@
 
 if [ -d "$GOPATH" ]; then
     echo "Uploading to Nexus..."
-	curl -u stratio:${NEXUSPASS} --upload-file $GOPATH/bin/dcos-oauth http://sodio.stratio.com/repository/paas/ansible/dcos-oauth/${VERSION}/
+	curl -u stratio:${NEXUSPASS} --upload-file $GOPATH/bin/dcos-oauth http://sodio.stratio.com/repository/paas/ansible/dcos-oauth-${VERSION}
 	echo "Finished uploading to Nexus."
 else
     echo "Target file not available, please run 'make compile' first"
