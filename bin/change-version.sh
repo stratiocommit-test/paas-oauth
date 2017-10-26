0e8bf53968378b312e7c6cb4dbfe81d652606481
pbedia-stratio <pbedia@stratio.com>
2017-10-26 10:49:31 +0200

    Revert GO workaround (#17)

--- bin/change-version.sh
+++ bin/change-version.sh
@@ -4,11 +4,5 @@ BASEDIR=`dirname $0`/..
 
 cd $BASEDIR
 
-### Awful workaround because of GO
-if [ -f "/home/jenkins/.ssh/config" ]; then
-    chmod 600 /home/jenkins/.ssh/config
-    chown jenkins /home/jenkins/.ssh/config
-fi
-
 echo "Modifying paas-oauth version to: $1"
 echo $1 > VERSION
